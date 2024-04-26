872c90ba3fc78f73c6282e18b1131bc407ca01d0
floresss27 <gfloreslima27@gmail.com>
2024-04-25 23:44:48 -0300

    feat: implement check password

---
+++ src/main/java/br/flores/gustavo/gestao_vagas/modules/company/dto/AuthCompanyDTO.java
@@ -0,0 +1,13 @@
+package br.flores.gustavo.gestao_vagas.modules.company.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthCompanyDTO {
+    
+    private username;
+    private password;
+
+}
--- src/main/java/br/flores/gustavo/gestao_vagas/modules/company/services/AuthCompanyService.java
+++ src/main/java/br/flores/gustavo/gestao_vagas/modules/company/services/AuthCompanyService.java
@@ -1,8 +1,36 @@
 package br.flores.gustavo.gestao_vagas.modules.company.services;
 
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import br.flores.gustavo.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import br.flores.gustavo.gestao_vagas.modules.company.repositories.CompanyRepository;
 
 @Service
 public class AuthCompanyService {
     
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public void execute(AuthCompanyDTO authCompanyDTO) {
+        var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow( () -> {
+            throw new UsernameNotFoundException("Company not found");
+        });
+        // Verificar se a senha são iguais
+        var passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
+
+        if (!passwordMatches) {
+            throw new AuthenticationException();
+        }
+            // Se não for igual -> Erro
+            // Se for igual -> Gera o Token
+        
+    }
+
 }
